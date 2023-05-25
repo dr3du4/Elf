c6a5b9824f64d022046c47f1545d3acdd67ff352
dr3du4 <91844190+dr3du4@users.noreply.github.com>
2023-05-25 16:25:55 +0200

    Add camera fix and start dialogue system

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+
+    public string[] lines;
+    private int index;
+    public float textSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = string.Empty; // should be in Awake
+        StartDialogue(); // this line should be executed after dialog button clicked
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+            
+        }
+        
+    }
+
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    
+
+}
